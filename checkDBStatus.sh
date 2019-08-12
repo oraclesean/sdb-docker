26792af4c5017213a7c64bc5f12ef8c1e6afa6cf
Sean <oracle.sean@gmail.com>
2019-08-12 11:02:45 -0600

    Fix for health check of standby database, particularly after a DG switch

--- checkDBStatus.sh
+++ checkDBStatus.sh
@@ -11,47 +11,74 @@
 #               2 = Sql Plus execution failed
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
 # 
+# Updated: August 2019
+# Author: oracle.sean@gmail.com
+# Description: Modifications for handling Data Guard container environments.
 
 ORACLE_SID="`grep $ORACLE_HOME /etc/oratab | cut -d: -f1`"
 OPEN_MODE="READ WRITE"
 ORAENV_ASK=NO
 source oraenv
 
-# Check Databases in the DG configuration that are ROLE=PRIMARY:
-
-if [[ "$ROLE" = "PRIMARY" ]]
-then
 # Check Oracle at least one PDB has open_mode "READ WRITE" and store it in status
-status=`sqlplus -s / as sysdba << EOF
+db_role=`sqlplus -s / as sysdba << EOF
    set heading off;
    set pagesize 0;
-   SELECT DISTINCT open_mode FROM v\\$pdbs WHERE open_mode = '$OPEN_MODE';
+   select database_role from v\\$database;
    exit;
 EOF`
 
 # Store return code from SQL*Plus
 ret=$?
 
-# SQL Plus execution was successful and PDB is open
-if [ $ret -eq 0 ] && [ "$status" = "$OPEN_MODE" ]; then
-   exit 0;
-# PDB is not open
-elif [ "$status" != "$OPEN_MODE" ]; then
-   exit 1;
-# SQL Plus execution failed
+# SQL Plus execution was successful and the database is PRIMARY:
+if [ $ret -eq 0 ] && [ "$db_role" = "PRIMARY" ]; then
+
+   # DB Role is PRIMARY; check for an open PDB
+   status=`sqlplus -s / as sysdba << EOF
+   set heading off;
+   set pagesize 0;
+   SELECT DISTINCT open_mode FROM v\\$pdbs WHERE open_mode = '$OPEN_MODE';
+   exit;
+EOF`
+
+   # Store return code from SQL*Plus
+   ret=$?
+
+   # SQL Plus execution was successful and PDB is open
+   if [ $ret -eq 0 ] && [ "$status" = "$OPEN_MODE" ]; then
+      exit 0;
+   # PDB is not open
+   elif [ "$status" != "$OPEN_MODE" ]; then
+      exit 1;
+   # SQL Plus execution failed
+   else
+      exit 2;
+   fi;
+   
+elif [ "$db_role" = "PHYSICAL STANDBY" ]; then
+   # DB Role is STANDBY; check for DB state 
+   status=`sqlplus -s / as sysdba << EOF
+   set heading off;
+   set pagesize 0;
+   select "HEALTHY" from v\\$database where open_mode in ('MOUNTED', 'READ ONLY', 'READ ONLY WITH APPLY');
+   exit;
+EOF`
+
+   # Store return code from SQL*Plus
+   ret=$?
+
+   # SQL Plus execution was successful and mode is in the list
+   if [ $ret -eq 0 ] && [ "$status" = "HEALTHY" ]; then
+      exit 0;
+   # Database mode is not in the list
+   elif [ "$status" != "HEALTHY" ]; then
+      exit 1;
+   # SQL Plus execution failed
+   else
+      exit 2;
+   fi;
+
 else
-   exit 2;
-fi;
-
-# Check databases in the DG configuration that are ROLE=STANDBY (they will have been started NOMOUNT):
-
-ret=$(ps -ef | grep pmon | grep ${ORACLE_SID} | grep -v grep | wc -l)
-
-if [ $ret -eq 1 ]; then
-   exit 0;
-else
-  exit 1;
-fi;
-
-fi;
-
+  exit 2;
+fi
